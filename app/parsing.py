c982bfaa54c80d7239bc448f0310f57c5e130687
ratmeow <rtm_bis@mail.ru>
2024-10-21 15:28:01 +0500

    added app folder

--- app/parsing.py
+++ app/parsing.py
@@ -24,6 +24,6 @@ for letter in tqdm(letters):
 print(len(all_nouns))
 
 
-with open("../data/words.txt", "w", encoding="utf-8") as file:
+with open("./data/words.txt", "w", encoding="utf-8") as file:
     for noun in all_nouns:
         file.write(noun + "\n")
